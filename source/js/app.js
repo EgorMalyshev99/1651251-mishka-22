f4e5de42b062489ac6f3ac90484b6eae89d07262
EgorMalyshev99 <egor.mlshv99@mail.ru>
2021-04-23 18:26:47 +0300

    adaptive tablet index 1.1

--- source/js/app.js
+++ source/js/app.js
@@ -2,6 +2,7 @@ const closeMenu__btn = document.querySelector(".main-nav__toggle");
 const mainNav = document.querySelector(".main-nav__inner");
 
 closeMenu__btn.addEventListener("click", (evt) => {
+  console.log("zzz");
   closeMenu__btn.classList.toggle("menu-closed");
   mainNav.classList.toggle("main-nav__closed");
 });
